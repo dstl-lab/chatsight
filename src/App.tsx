59cb741eadbc719714ebbf20780ddb089b530f42
m1nce <miinchaankiim@gmail.com>
2025-12-16 21:43:14 +0900

    created header

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import dstlLogo from './assets/dstl-logo.png';
+import GridLayout, { Layout } from 'react-grid-layout';
+import 'react-grid-layout/css/styles.css';
+import 'react-resizable/css/styles.css';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="header">
+        <div className="header-left">
+          <p>File</p>
+          <p>Help</p>
+          <p>Documentation</p>
+          <p>Save</p>
+        </div>
+        <div className="header-center">ChatSight</div>
+        <div className="header-right">
+          <img src={dstlLogo} alt="DSTL Logo" />
+          <p>dstl lab</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="header-separator" />
     </>
   )
 }
