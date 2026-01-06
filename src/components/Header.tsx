6d6f92df7f33033fe089cacec5f1278eb39644d2
m1nce <miinchaankiim@gmail.com>
2026-01-06 15:49:19 -0800

    remove dependency

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import { apiClient, type FileData } from '../services/apiClient';
+import { apiClient } from '../services/apiClient';
 import dstlLogo from '../assets/dstl-logo.png';
 import './Header.css';
 
